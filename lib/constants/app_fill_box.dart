779e04ae9d37a5e158274264bec0d8a0bb7b6ef9
lihuiyong1 <lihuiyong1@newhope.com>
2024-10-14 18:07:47 +0800

    add loading

---
+++ lib/constants/app_fill_box.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/material.dart';
+
+class AppFillBox{
+
+  static const gapH4 = SizedBox(height: 4);
+  static const gapH8 = SizedBox(height: 8);
+  static const gapH16 = SizedBox(height: 16);
+  static const gapH20 = SizedBox(height: 20);
+  static const gapH24 = SizedBox(height: 24);
+
+  static const gapW4 = SizedBox(width: 4);
+  static const gapW8 = SizedBox(width: 8);
+  static const gapW16 = SizedBox(width: 16);
+  static const gapW20 = SizedBox(width: 20);
+  static const gapW24 = SizedBox(width: 24);
+}
--- lib/pages/loading/loading.dart
+++ lib/pages/loading/loading.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:flutter_china_stats_visualizer/constants/app_fill_box.dart';
+import 'package:flutter_china_stats_visualizer/theme/app_colors.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class Loading extends StatelessWidget{
@@ -14,7 +16,17 @@ class Loading extends StatelessWidget{
                     child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                            LoadingAnimationWidget.discreteCircle(color: Colors.white,size: 100)
+                            LoadingAnimationWidget.discreteCircle(
+                                color: AppColors.primary,
+                                size: 100
+                            ),
+                            AppFillBox.gapH16,
+                            Text("NBSC DATA INITING...",
+                            style: TextStyle(
+                                fontSize: Theme.of(context).textTheme.bodyLarge?.fontSize,
+                                fontWeight: FontWeight.w600,
+                                color: Theme.of(context).textTheme.bodyLarge?.color
+                            ),)
                         ],
 
                     ),
