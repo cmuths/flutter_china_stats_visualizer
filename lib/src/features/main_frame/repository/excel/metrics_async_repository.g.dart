6d8656469fad7a8e39183ad99260c4ff3c79f3bd
lihuiyong1 <lihuiyong1@newhope.com>
2024-10-18 17:28:33 +0800

    add sqlite3

---
+++ lib/src/features/main_frame/repository/excel/metrics_async_repository.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'metrics_async_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$metricsAsyncRepositoryHash() =>
+    r'5747229213f22babdc2ac624b2f9f69e20c7fb8a';
+
+/// See also [MetricsAsyncRepository].
+@ProviderFor(MetricsAsyncRepository)
+final metricsAsyncRepositoryProvider = AutoDisposeAsyncNotifierProvider<
+    MetricsAsyncRepository, List<MetricsDefineDomain>>.internal(
+  MetricsAsyncRepository.new,
+  name: r'metricsAsyncRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$metricsAsyncRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MetricsAsyncRepository
+    = AutoDisposeAsyncNotifier<List<MetricsDefineDomain>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter, deprecated_member_use_from_same_package
